5c57a4c3b86995b15593135bb5ac18b5e94962b1
Nick-tsoukas <nick.tsoukas101@gmail.com>
2017-10-06 10:31:35 -0400

    server response

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -14,14 +14,22 @@
   RemoteDataStore.prototype.add = function (key, val) {
     $.post(this.serverUrl, val, function (serverResponse) {
       console.log(serverResponse);
+      cb(serverResponse);
     });
   };
 
-  RemoteDataStore.prototype.getAll = function () {
+  RemoteDataStore.prototype.getAll = function (cb) {
     $.get(this.serverUrl, function (serverResponse) {
       console.log(serverResponse);
     })
-  }
+  };
+
+  RemoteDataStore.prototype.get = function (key, cb) {
+    $.get(this.serverUrl + '/' + key, function (serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
+    });
+  };
 
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
